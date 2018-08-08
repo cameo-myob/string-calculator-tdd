58ba856e549a0f2f80871eb349f58a9005536207
Cameo Langford <cameo.langford@c02wp0skhtd8.myob.myobcorp.net>
2018-08-08 10:31:15 +1000

    Second test passing

--- test/com/company/CalculatorTest.java
+++ test/com/company/CalculatorTest.java
@@ -14,4 +14,15 @@ class CalculatorTest {
         assertEquals(test, Calculator.add("0"));
     }
 
+//    Second test - method takes string value and returns int value
+    @Test
+    public void returnsIntValue(){
+        int test2 = 2;
+        int test3 = 3;
+        int test10 = 10;
+
+        assertEquals(test2, Calculator.add(Integer.toString(test2)));
+        assertEquals(test3, Calculator.add(Integer.toString(test3)));
+        assertEquals(test10, Calculator.add(Integer.toString(test10)));
+    }
 }
